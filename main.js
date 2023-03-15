96e66ec3d93caf4ab0e7e65015fd0ef6bee35138
Federico <federicoaballay222@gmail.com>
2023-03-15 14:34:39 -0300

    subida de preentrega1

---
+++ main.js
@@ -0,0 +1,20 @@
+let nombre = prompt ("Ingresa tu nombre")
+
+
+function saludo () {
+    if (nombre == ""){
+        alert ("No ingresaste tu nombre")
+        for (let nombre = 0; nombre <= 100; nombre ++){
+            let nombre = prompt ("Solo tenes que escribir tu nombre o este mensaje va a aparecer para siempre")
+            if (nombre != ""){
+                alert ("Bienvenido" + " " + nombre + " "+ "tan dificil era?")
+                break
+            }
+        }
+    }else{
+        alert ("Bienvenido" + " " + nombre + " " +"que pro")
+    }
+    }
+
+saludo()
+
