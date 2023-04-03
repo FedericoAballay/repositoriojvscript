0cf03d2fc12b2127f4fed93ea3edc3070aabbc7b
Federico <federicoaballay222@gmail.com>
2023-04-02 21:10:01 -0300

    Finalizada segunda preentrega

--- main.js
+++ main.js
@@ -1,27 +1,143 @@
-let nombre = prompt ("Ingresa tu nombre")
+const catalogo = [
+{
+    nombre:"remera Fila",
+    talle:"m",
+    id:"remera",
+    codigo:"r1",
+    precio:1000,
+},
+{
+    nombre:"remera Nike",
+    talle:"l",
+    id:"remera",
+    codigo:"r2",
+    precio:1000,
+},
+{
+    nombre:"remera Adidas",
+    talle:"xl",
+    id:"remera",
+    codigo:"r3",
+    precio:1000,
+},
+{
+    nombre:"buzo Fila",
+    talle:"m",
+    id:"buzo",
+    codigo:"b1",
+    precio:5000,
+},
+{
+    nombre:"buzo Nike",
+    talle:"l",
+    id:"buzo",
+    codigo:"b2",
+    precio:5000,
+},
+{
+    nombre:"buzo Adidas",
+    talle:"xl",
+    id:"buzo",
+    codigo:"b3",
+    precio:5000,
+},
+{
+    nombre:"pantalon Fila",
+    talle:"s",
+    id:"pantalon",
+    codigo:"p1",
+    precio:3000,
+},
+{
+    nombre:"pantalon Nike",
+    talle:"l",
+    id:"pantalon",
+    codigo:"p2",
+    precio:3000,
+},
+{
+    nombre:"pantalon Adidad",
+    talle:"xl",
+    id:"pantalon",
+    codigo:"p3",
+    precio:3000,
+},
 
+];
 
+const remeras = catalogo.filter((producto)=>producto.id === "remera");
+const buzos = catalogo.filter((producto)=>producto.id === "buzo");
+const pantalones = catalogo.filter((producto)=>producto.id === "pantalon");
 
+let filtro = prompt ('ingresa:\n1) para remeras \n2) para buzos \n3) para pantalones')
 
-function saludo () {
-    if (nombre == ""){
-        alert ("No ingresaste tu nombre")
-        for (let nombre = 0; nombre <= 100; nombre ++){
-            let nombre = prompt ("Solo tenes que escribir tu nombre o este mensaje va a aparecer 100 veces")
-            if (nombre != ""){
-                alert ("Bienvenido" + " " + nombre + " "+ "tan dificil era?")
-                break
-            }
+function saludo(){
+    if (filtro == "1"){
+        for (const item of remeras){
+            console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/${item.precio}`)
         }
-    }else{
-        alert ("Bienvenido" + " " + nombre + " " +"que pro")
-        
     }
+    if (filtro == "2"){
+        for (const item of buzos){
+            console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/${item.precio}`)
+        }
     }
-
+    if (filtro == "3"){
+        for (const item of pantalones){
+            console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/${item.precio}`)
+        }
+    }
+    
+}
 saludo()
 
+let carrito = [];
+
+function comprar(){
+    let buy = prompt ("Ingresa el codigo del producto que deseas comprar")
+    console.log(catalogo.find((producto)=> buy === producto.codigo))
+    carrito.push(buy)
+    console.log(carrito)
+}
+
+comprar()
+
+function comprarMas(){
+    let continuarCompra = prompt ("Desea continuar comprando?\n 1)Si\n 2)No\n 3)Deseas comprar otro tipo de prenda")
+    if (continuarCompra == "1") {
+        let buy = prompt ("Ingresa el codigo del producto que deseas comprar")
+    console.log(catalogo.find((producto)=> buy === producto.codigo))
+    carrito.push(buy)
+    console.log(carrito)
+    comprarMas()
+    }
+    
+    if (continuarCompra == "2") {
+        alert("Gracias por su compra")
+    }
+    if (continuarCompra == "3") {
+        let filtro = prompt ('ingresa:\n1) para remeras \n2) para buzos \n3) para pantalones')
+        if (filtro == "1"){
+            for (const item of remeras){
+                console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/precio:$${item.precio}`)
+            }
+        }
+        if (filtro == "2"){
+            for (const item of buzos){
+                console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/precio:$${item.precio}`)
+            }
+        }
+        if (filtro == "3"){
+            for (const item of pantalones){
+                console.log(`${item.nombre}/${item.talle}/${item.id}/codigo:${item.codigo}/precio:$${item.precio}`)
+            }
+        }
+        comprar()
+        comprarMas()
+    }
+}
 
 
 
+comprarMas()
 
